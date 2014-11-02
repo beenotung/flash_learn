03c0b428af18a4ab636c55119de36d7045e58317
Beeno Tung <abbcc1241@yahoo.com.hk>
2014-11-02 12:57:50 +0800

    finished addvocab gui part

--- src/flashlearn/JPanelAddVocab.java
+++ src/flashlearn/JPanelAddVocab.java
@@ -2,8 +2,6 @@ package flashlearn;
 
 import javax.swing.JPanel;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.TextField;
 import java.awt.Choice;
@@ -11,8 +9,6 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Vector;
 
-import javax.swing.BoxLayout;
-import javax.swing.JFrame;
 import javax.swing.JTextField;
 import javax.swing.JButton;
 
@@ -20,67 +16,81 @@ import javax.swing.JButton;
 public class JPanelAddVocab extends JPanel {
 	private FlashLearnSwing flashLearnSwing;
 
-	private JTextField textFieldWord;
 	private static String strAddLabel = "Add Label";
 	private static String strAddWord = "Add Word";
 	private static String strLabelDefault = "Type Label Here";
 	private static String strLangDefault = "Choose Lang Here";
 	private static String strWordDefault = "Type Word Here";
 
+	private JButton btnAddLabel;
+	private JButton btnAddPanelWord;
+	private Vector<JPanel> panelLabels;
+	private Vector<JPanel> panelWords;
+
 	/**
 	 * Create the panel.
 	 * 
 	 * @param flashLearnSwing
 	 */
+
 	public JPanelAddVocab(FlashLearnSwing flashLearnSwing) {
 		this.flashLearnSwing = flashLearnSwing;
 
 		setLayout(new FlowLayout());
 
-		JButton btnAddLabel = new JButton(strAddLabel);
-		add(btnAddLabel);
+		btnAddLabel = new JButton(strAddLabel);
 		btnAddLabel.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				addLabel();
 			}
 		});
 
-		JButton btnAddPanelWord = new JButton(strAddWord);
-		add(btnAddPanelWord);
+		btnAddPanelWord = new JButton(strAddWord);
 		btnAddPanelWord.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				addWord();
 			}
 		});
 
-		addLabel();
+		panelLabels = new Vector<JPanel>();
+		panelWords = new Vector<JPanel>();
 
+		addLabel();
 		addWord();
 	}
 
 	protected void addLabel() {
-		// TODO Auto-generated method stub
 		JPanel panelLabel = new JPanel();
-		add(panelLabel);
-		// panelLabels.add(panelLabel);
+		panelLabels.add(panelLabel);
 		TextField textFieldLabel = new TextField(strLabelDefault);
 		panelLabel.add(textFieldLabel);
 
-		flashLearnSwing.update();
+		update();
 	}
 
 	protected void addWord() {
 		// TODO Auto-generated method stub
 		JPanel panelWord = new JPanel();
-		add(panelWord);
+		panelWords.add(panelWord);
 		Choice choiceLang = new Choice();
 		panelWord.add(choiceLang);
 		choiceLang.add(strLangDefault);
 
-		textFieldWord = new JTextField(strWordDefault);
+		JTextField textFieldWord = new JTextField(strWordDefault);
 		panelWord.add(textFieldWord);
 		textFieldWord.setColumns(10);
 
+		update();
+	}
+
+	private void update() {
+		removeAll();
+		add(btnAddLabel);
+		add(btnAddPanelWord);
+		for (JPanel jPanel : panelLabels)
+			add(jPanel);
+		for (JPanel jPanel : panelWords)
+			add(jPanel);
 		flashLearnSwing.update();
 	}
 
